1cbe1462063bdcc286cc20e33b1fcd68e234f74d
Shivendra <shivendra.tripathi@mca.christuniversity.in>
2025-01-02 12:46:22 +0530

    trying to see new logs

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,17 @@
-
-
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import sidebar from './Sidebaritems'
+import Nav from './Nav'
+
 
 function App() {
-  const [open,setopen]=useState(false)
-
-
-  return (
-    <>
-    <div className='border-2 w-2/4 border-red-500'>
-    
-    <div>{open && sidebar.map((a)=>{
-return <button className='flex flex-col h-12 border rounded-sm w-2/12 font-thin p-2'>{a.name}</button>
-    })}</div>
-    </div>
-    <button className='h-12 w-20 border-2 border-black m-2' onClick={()=>{
-      setopen(true)
-    }}>open this </button>
-    <button className='h-12 w-20 border-2 border-black' onClick={()=>{
-      setopen(false)
-    }}>close this</button>
-      </>
-  )
+ 
+  return <>
+ <Nav head1="Product" head2="features" head3="pricing" head4="Resources" head5="Contact sales"/>
+  <div>this is a new commit</div>
+  </>
 }
 
 export default App
